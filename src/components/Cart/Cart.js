705764bd155a1aab79e8b20f09d7de4bd2ece7ee
Khalid Saifullah <khalid0132@gmail.com>
2021-02-26 15:01:30 +0100

    cart, tax, total

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import React from "react";
 
 const Cart = (props) => {
-    // console.log(props.price);
-    const Cart = props.cart;
-    console.log(Cart);
-    return (
-        <div>
-            <h2>Order Summery</h2>
-            <p>Items ordered: {Cart.length}</p>
-        </div>
-    );
+  // console.log(props.price);
+  const cart = props.cart;
+  console.log(cart);
+  //   const totalPrice = Cart.reduce(( total,prd) => total+ prd.price,0);
+  let total = 0; // reduce a totalPrice used
+  for (let i = 0; i < cart.length; i++) {
+    const product = cart[i];
+    total = (total + product.price);
+  }
+
+  let shipping = 0;
+  if(total > 35){
+      shipping = 0;
+  }
+  else if(total >15){
+      shipping = 4.99;
+  }
+  else if(total > 0){
+      shipping = 12.99;
+  }
+
+  const tax = (total / 10).toFixed(2);
+
+  const grandTotal = (total + shipping + Number(tax)).toFixed(2);
+  return (
+    <div>
+      <h2>Order Summery</h2>
+      <p>Items ordered: {cart.length}</p>
+      <p>
+        <small>Product Price: {total}</small>
+      </p>
+      <p><small>Shipping: {shipping}</small></p>
+      <p><small>Tax: {tax}</small></p>
+    <p><small>Total Price: {grandTotal}</small></p>
+
+
+      {/* <p>
+        <small>Items:</small>
+      </p>
+      <p>
+        <small>Shipping and Handling:</small>
+      </p>
+      <p>
+        <small>Total before tax:{totalPrice}</small>
+      </p>
+      <p>
+        <small>Estimated Tax:</small>
+      </p> */}
+
+      <h2>Order Total</h2>
+      <button>Review your order</button>
+    </div>
+  );
 };
 
 export default Cart;
