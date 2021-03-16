b1e15e139af3eab74fc340cb2df7bed0ad5f629f
Khalid Saifullah <khalid0132@gmail.com>
2021-03-17 00:48:15 +0100

    commited

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../App";
+
 import logo from "../../images/logo.png";
 import "./Header.css";
 const Header = () => {
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
   return (
     <div className="header">
       {/* <h1>I am header</h1> */}
@@ -11,6 +14,7 @@ const Header = () => {
         <Link to="/Shop">Shop</Link>
         <Link to="/Review">Order Review</Link>
         <Link to="/Inventory">Manage Inventory</Link>
+      <button onClick= {()=> setLoggedInUser({})}>Sign out</button>
       </nav>
       <div className = "search-input">
           <span><input placeholder= "type here to search" type="text"/ ></span>
--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,10 +1,24 @@
-import React from 'react';
+// module-43-9
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { UserContext } from '../../App';
+import './Shipment.css';
 
 const Shipment = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => console.log(data);
+  
+    console.log(watch("example")); // watch input value by passing the name of it
+  
     return (
-        <div>
-            <h1>This is shipment</h1>
-        </div>
+      <form className = "ship-form" onSubmit={handleSubmit(onSubmit)}>
+        <input name="example" defaultValue = {loggedInUser.name} ref={register} />
+        <input name="exampleRequired" defaultValue = {loggedInUser.email} ref={register({ required: true })} />
+        {errors.exampleRequired && <span style={{color:'red'}}>This field is required</span>}
+        
+        <input type="submit" />
+      </form>
     );
 };
 
